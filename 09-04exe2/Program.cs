729db713eb76e0bd3dbfabb32a18d2957ed0a8c7
unknown <yourGitHub@example.com>
2020-04-10 20:58:53 +0500

    Added all programs

--- 09-04exe2/Program.cs
+++ 09-04exe2/Program.cs
@@ -6,16 +6,28 @@ namespace _09_04exe2
     {
         static void Main(string[] args)
         {
-            
+           Player pl=new Player();
+           pl.Play();
+           (pl as IPlayable).Stop();
+           pl.Record();
+           (pl as IRecodable).Stop();
+           Console.ReadKey();
         }
-        interface Iplayable
-        {
-            void Play(); void Pause(); void stop();
-        }
-        
-        interface IRecordable
-        {
-            void Record(); void Pause(); void stop();
-        }
+    }
+    interface IPlayable 
+    {
+        void Play();  void Pause(); void Stop();
+    }
+    interface IRecodable
+    {
+        void Record();  void Pause(); void Stop();
+    }
+    public class Player: IPlayable, IRecodable
+    {
+        public void Play() => Console.WriteLine("Play the music!");
+        public void Pause() => Console.WriteLine("Pause the music!");
+        public void Stop() => Console.WriteLine("Stop the music!");
+        public void Record() => Console.WriteLine("Record the music!");
+
     }
 }
